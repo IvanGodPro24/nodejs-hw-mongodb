4986546d720d743b0534dff5d5bf23fa12d03c98
IvanGodPro24 <vanyanep@gmail.com>
2025-03-04 17:04:26 +0200

    Add routes

---
+++ src/routes/contacts.js
@@ -0,0 +1,39 @@
+import { Router } from 'express';
+import { getAllContacts, getContactById } from '../services/contacts.js';
+
+const router = Router();
+
+router.get('/contacts', async (req, res) => {
+  const contacts = await getAllContacts();
+
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully found contacts!',
+    data: contacts,
+  });
+});
+
+router.get('/contacts/:contactId', async (req, res, next) => {
+  const { contactId } = req.params;
+
+  try {
+    const contact = await getContactById(contactId);
+
+    if (!contact) {
+      res.status(404).json({
+        message: 'Contact not found',
+      });
+      return;
+    }
+
+    res.status(200).json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default router;
